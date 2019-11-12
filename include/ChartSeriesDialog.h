425944a8082de11303ccd23c301c72dfc105d9ef
mrlukasbos <mrlukasbos@gmail.com>
2019-11-12 11:27:44 +0100

    start with visualization settings

--- include/ChartSeriesDialog.h
+++ include/ChartSeriesDialog.h
@@ -10,10 +10,12 @@ class ChartSeriesDialog : public QDialog {
   QVBoxLayout * network_settings_layout;
   QComboBox * channel_combo_box;
   QWidget * create_network_settings_tab(ChartSeries * series);
+  QWidget * create_visualization_settings_tab(ChartSeries * series);
+
  public:
   explicit ChartSeriesDialog(ChartSeries * series);
  public slots:
-  void create_filter_view(QVBoxLayout *current_filters_layout, const Filter &filter) const;
+  QWidget * create_filter_widget(QLayout * parent, const Filter &filter) const;
 };
 
 #endif //RTT_CHARTSERIESDIALOG_H
--- src/ChartSeriesDialog.cpp
+++ src/ChartSeriesDialog.cpp
@@ -4,86 +4,89 @@
 #include <include/AddFilterDialog.h>
 #include "ChartSeriesDialog.h"
 
-ChartSeriesDialog::ChartSeriesDialog(ChartSeries * series) : QDialog(series) {
-  setMinimumWidth(600);
-  auto dialog_layout = new QVBoxLayout(this);
-  setLayout(dialog_layout);
-  auto tab_widget = new QTabWidget();
-  dialog_layout->addWidget(tab_widget);
-  QWidget * network_settings_tab = create_network_settings_tab(series);
-  tab_widget->addTab(network_settings_tab, "Data");
+ChartSeriesDialog::ChartSeriesDialog(ChartSeries *series) : QDialog(series) {
+    setMinimumWidth(600);
+    auto dialog_layout = new QVBoxLayout(this);
+    setLayout(dialog_layout);
+    auto tab_widget = new QTabWidget();
+    dialog_layout->addWidget(tab_widget);
+    QWidget *network_settings_tab = create_network_settings_tab(series);
+    tab_widget->addTab(network_settings_tab, "Input");
+
+    QWidget * visualization_tab = create_visualization_settings_tab(series);
+    tab_widget->addTab(visualization_tab, "Visualization");
 }
 
-QWidget * ChartSeriesDialog::create_network_settings_tab(ChartSeries * series) {
-  auto network_settings_tab = new QWidget();
-  network_settings_layout = new QVBoxLayout();
-  network_settings_tab->setLayout(network_settings_layout);
+QWidget *ChartSeriesDialog::create_network_settings_tab(ChartSeries *series) {
+    auto network_settings_tab = new QWidget();
+    network_settings_layout = new QVBoxLayout();
+    network_settings_tab->setLayout(network_settings_layout);
 
-  // communication layout
-  auto comm_layout = new QHBoxLayout();
-  channel_combo_box = new QComboBox();
-  comm_layout->addWidget(channel_combo_box);
-  for (auto const &channel : proto::CHANNELS) {
-      channel_combo_box->addItem(QString::fromStdString(channel.second.name));
-  }
-    connect(channel_combo_box, &QComboBox::currentTextChanged, [this](QString text) {
-        selected_topic_name = text;
+    // communication layout
+    auto comm_layout = new QHBoxLayout();
+    channel_combo_box = new QComboBox();
+    comm_layout->addWidget(channel_combo_box);
+    for (auto const &channel : proto::CHANNELS) {
+        channel_combo_box->addItem(QString::fromStdString(channel.second.name));
+    }
+    connect(channel_combo_box, &QComboBox::currentTextChanged, [this](const QString &text) {
+      selected_topic_name = text;
     });
 
     network_settings_layout->addLayout(comm_layout);
 
+    auto current_filters_label = new QLabel();
+    current_filters_label->setText("Active filters");
+    network_settings_layout->addWidget(current_filters_label);
 
-  auto current_filters_label = new QLabel();
-  current_filters_label->setText("Active filters");
-  network_settings_layout->addWidget(current_filters_label);
-
-  auto current_filters_layout = new QVBoxLayout();
-  for (auto filter : series->get_filters()) {
-      create_filter_view(current_filters_layout, filter);
-  }
-
-
+    auto current_filters_layout = new QVBoxLayout();
+    for (auto filter : series->get_filters()) {
+        auto filter_widget = create_filter_widget(current_filters_layout, filter);
+        current_filters_layout->addWidget(filter_widget);
+    }
     network_settings_layout->addLayout(current_filters_layout);
 
     auto add_new_filter_button = new QPushButton();
     add_new_filter_button->setText("Add new filter");
     connect(add_new_filter_button, &QPushButton::clicked, [this, series, current_filters_layout]() {
-        auto new_filter = series->add_new_filter();
-        create_filter_view(current_filters_layout, *new_filter);
+      auto new_filter = series->add_new_filter();
+      auto filter_widget = create_filter_widget(current_filters_layout, *new_filter);
+      current_filters_layout->addWidget(filter_widget);
     });
     network_settings_layout->addWidget(add_new_filter_button);
 
+    auto buttons_layout = new QHBoxLayout();
+    network_settings_layout->addLayout(buttons_layout);
 
-  auto buttons_layout = new QHBoxLayout();
-  network_settings_layout->addLayout(buttons_layout);
+    // Cancel button
+    auto cancel_button = new QPushButton();
+    cancel_button->setText("Cancel");
+    buttons_layout->addWidget(cancel_button);
+    connect(cancel_button, &QPushButton::clicked, [this]() {
+      this->close();
+    });
 
-  // Cancel button
-  auto cancel_button = new QPushButton();
-  cancel_button->setText("Cancel");
-  buttons_layout->addWidget(cancel_button);
-  connect(cancel_button, &QPushButton::clicked, [this]() {
-    this->close();
-  });
+    // Apply button
+    auto apply_network_settings_button = new QPushButton();
+    apply_network_settings_button->setText("Apply");
+    apply_network_settings_button->setStyleSheet("background-color: green;");
+    buttons_layout->addWidget(apply_network_settings_button);
 
-  // Apply button
-  auto apply_network_settings_button = new QPushButton();
-  apply_network_settings_button->setText("Apply");
-  apply_network_settings_button->setStyleSheet("background-color: green;");
-  buttons_layout->addWidget(apply_network_settings_button);
-
-  connect(apply_network_settings_button, &QPushButton::clicked, [this, series]() {
-    for (auto const &channel : proto::CHANNELS) {
-        if (channel_combo_box->currentText() == QString::fromStdString(channel.second.name)) {
-            series->update_channel(channel.first);
-            this->close();
-        }
-    }
-  });
-  return network_settings_tab;
+    connect(apply_network_settings_button, &QPushButton::clicked, [this, series]() {
+      for (auto const &channel : proto::CHANNELS) {
+          if (channel_combo_box->currentText()==QString::fromStdString(channel.second.name)) {
+              series->update_channel(channel.first);
+              this->close();
+          }
+      }
+    });
+    return network_settings_tab;
 }
 
-void ChartSeriesDialog::create_filter_view(QVBoxLayout *current_filters_layout, const Filter &filter) const {
+QWidget *ChartSeriesDialog::create_filter_widget(QLayout *parent, const Filter &filter) const {
+    auto filter_widget = new QWidget();
     auto filter_layout = new QHBoxLayout();
+    filter_widget->setLayout(filter_layout);
     auto add_filter_dialog = new AddFilterDialog((QWidget *) this);
     auto add_filter_button = new QPushButton();
     if (filter.field_descriptor) {
@@ -95,14 +98,42 @@ void ChartSeriesDialog::create_filter_view(QVBoxLayout *current_filters_layout, 
     connect(add_filter_dialog, &AddFilterDialog::finished, [=]() {
       add_filter_button->setText(QString::fromStdString(add_filter_dialog->get_selected_field_descriptor()->name()));
     });
-    connect(channel_combo_box, &QComboBox::currentTextChanged, add_filter_dialog, &AddFilterDialog::update_filters_layout);
-    if (selected_topic_name != "") {
+    connect(channel_combo_box,
+            &QComboBox::currentTextChanged,
+            add_filter_dialog,
+            &AddFilterDialog::update_filters_layout);
+    if (selected_topic_name!="") {
         add_filter_dialog->update_filters_layout(selected_topic_name);
     }
     filter_layout->addWidget(add_filter_button);
+
     auto filter_value_edit = new QLineEdit();
     filter_layout->addWidget(filter_value_edit);
-    current_filters_layout->addLayout(filter_layout);
+
+    auto rm_filter_button = new QPushButton();
+    rm_filter_button->setText("Remove...");
+    return filter_widget;
+}
+
+QWidget *ChartSeriesDialog::create_visualization_settings_tab(ChartSeries *series) {
+    auto visualization_widget = new QWidget();
+    auto visualization_layout = new QHBoxLayout();
+    visualization_widget->setLayout(visualization_layout);
+
+    QGroupBox *groupBox = new QGroupBox(tr("Data to plot"));
+    QRadioButton *radio_rate = new QRadioButton("Data rate (packets/s)");
+    QRadioButton *radio_field = new QRadioButton("Custom field");
+    radio_rate->setChecked(true);
+
+    QVBoxLayout *vbox = new QVBoxLayout;
+    vbox->addWidget(radio_rate);
+    vbox->addWidget(radio_field);
+    vbox->addStretch(1);
+    groupBox->setLayout(vbox);
+
+    visualization_layout->addWidget(groupBox);
+
+    return visualization_widget;
 }
 
 
